b5d5392387c2cbbf62f14db3bc8b27c2657599ff
Min-Yen Kan <kanmy@comp.nus.edu.sg>
2014-07-15 16:19:17 +0800

    Done with Intro

--- SwiftTutorial.2.playground/section-1.swift
+++ SwiftTutorial.2.playground/section-1.swift
@@ -21,3 +21,37 @@ if onSaleInferred {
 } else {
     println("\(nameInferred) at regular price: \(priceInferred)!")
 }
+
+/** Classes and Methods
+*/
+// 1
+class TipCalculator {
+    
+    // 2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+
+    // 3
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    // 4
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    // 5
+    func printPossibleTips() {
+        println("15%: \(calcTipWithTipPct(0.15))")
+        println("18%: \(calcTipWithTipPct(0.18))")
+        println("20%: \(calcTipWithTipPct(0.20))")
+    }
+}
+
+// 6
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.printPossibleTips()
